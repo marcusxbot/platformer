51235299c0f017cbefa42ff2a083aa457d751c3e
Marcus Salér <marcus.saler@elev.ga.lbs.se>
2018-12-07 14:18:06 +0100

    Restar

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+    public string RestartGame = "nivå1";
+	
+	void Update () // Update is called once per frame
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(RestartGame); //loads scene
+        }
+    }
+
+}
